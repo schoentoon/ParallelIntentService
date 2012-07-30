aff3c09c789a1bbae73b02e12adbe0b67df29354
Toon Schoenmakers <nighteyes1993@gmail.com>
2012-07-30 16:01:44 +0200

    Implemented the actual Parallel Intent Service, including an example

--- src/com/schoentoon/parallel/intentservice/MainActivity.java
+++ src/com/schoentoon/parallel/intentservice/MainActivity.java
@@ -2,9 +2,14 @@ package com.schoentoon.parallel.intentservice;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
+  private static final String TAG = MainActivity.class.getSimpleName();
+
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
@@ -14,5 +19,12 @@ public class MainActivity extends Activity {
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.activity_main, menu);
     return true;
+  }
+
+  public void onStartService(View v) {
+    Log.d(TAG,"onStartService(View);");
+    Intent intent = new Intent(this, TestService.class);
+    for (int i = 0; i < 5; i++)
+      startService(intent);
   }
 }
